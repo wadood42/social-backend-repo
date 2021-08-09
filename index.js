484ba2c8749fe2f01181e3d0f8ab597af3352bc7
Abdul <wbehzad12@gmail.com>
2021-08-09 13:01:04 -0500

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const helmet = require("helmet");
 const app = express();
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
@@ -8,8 +7,13 @@ const conversationRoutes = require("./routes/conversations");
 const messageRoutes = require("./routes/messages");
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./configs/.env" });
-
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "https://optimistic-curran-eb4106.netlify.app",
+  optionsSuccessStatus: 200,
+};
 
 mongoose
   .connect(process.env.MONGO_DB_URI, {
@@ -26,8 +30,9 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors(corsOptions));
 // ROUTES FOR USER
+
 app.use("/api/conversations", conversationRoutes);
 app.use("/api/messages", messageRoutes);
 
